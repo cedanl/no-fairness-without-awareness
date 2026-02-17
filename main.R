634778e2d475ec62ff9ec5358ccebafb4f9d8e56
aslamht <a.hanif.tanjung@gmail.com>
2026-02-17 14:43:57 +0100

    feat: Do not update packages when testing

--- main.R
+++ main.R
@@ -22,7 +22,7 @@
 # Laad het NFWA package
 # Voor development: gebruik devtools::load_all()
 # Na installatie: library(nfwa)
-devtools::install()
+devtools::install(upgrade = "never")
 
 # Installeer benodigde dependencies (alleen eerste keer)
 if (!tinytex::is_tinytex()) {
